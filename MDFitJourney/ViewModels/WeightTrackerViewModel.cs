e8a98662302e366ca65c4bb6887cd59f13e490ca
Xuhui Zou <zouxi@mail.uc.edu>
2025-08-04 14:39:56 -0400

    Weight edit bug fixed

--- MDFitJourney/ViewModels/WeightTrackerViewModel.cs
+++ MDFitJourney/ViewModels/WeightTrackerViewModel.cs
@@ -2,76 +2,58 @@
 using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System;
 
 namespace MDFitJourney.ViewModels
 {
-    public class WeightEntry
+    public partial class WeightEntry : ObservableObject
     {
-        public DateTime Date { get; set; }
-        public double Weight { get; set; }
-    }
+        [ObservableProperty]
+        private DateTime date;
 
+        [ObservableProperty]
+        private double weight;
+    }
 
     public partial class WeightTrackerViewModel : ObservableObject
     {
-
         [ObservableProperty]
         private string currentWeight;
 
         [ObservableProperty]
         private DateTime selectedDate = DateTime.Today;
 
-
         public ObservableCollection<WeightEntry> WeightEntries { get; set; } = new ObservableCollection<WeightEntry>();
-
-        public WeightTrackerViewModel()
-        {
-            
-        }
 
 
         [RelayCommand]
         private void SaveWeight()
         {
-            
             if (double.TryParse(CurrentWeight, out double weightValue))
             {
-                
                 var existingEntry = WeightEntries.FirstOrDefault(e => e.Date.Date == SelectedDate.Date);
 
                 if (existingEntry != null)
                 {
-                    
                     existingEntry.Weight = weightValue;
-                    
-                    
-                    var temp = WeightEntries.ToList();
-                    WeightEntries.Clear();
-                    foreach (var entry in temp.OrderBy(e => e.Date))
-                    {
-                        WeightEntries.Add(entry);
-                    }
                 }
                 else
                 {
-                    
                     var newEntry = new WeightEntry
                     {
                         Date = SelectedDate.Date,
                         Weight = weightValue
                     };
-                    
-                    
                     WeightEntries.Add(newEntry);
-                    var temp = WeightEntries.ToList();
-                    WeightEntries.Clear();
-                    foreach (var entry in temp.OrderBy(e => e.Date))
-                    {
-                        WeightEntries.Add(entry);
-                    }
                 }
-                
-                
+
+                var sortedEntries = WeightEntries.OrderBy(e => e.Date).ToList();
+                WeightEntries.Clear();
+                foreach (var entry in sortedEntries)
+                {
+                    WeightEntries.Add(entry);
+                }
+
                 CurrentWeight = string.Empty;
                 SelectedDate = DateTime.Today;
             }
@@ -80,8 +62,7 @@ namespace MDFitJourney.ViewModels
                 Console.WriteLine("Invalid weight format. Please enter a number.");
             }
         }
-        
-     
+
         [RelayCommand]
         private void EditWeight(WeightEntry entry)
         {
